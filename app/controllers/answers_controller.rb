786b8af62c834301c651b09dab24d6ec534f0f25
Kenchi247 <inmf78bk247@gmail.com>
2019-10-08 11:12:59 +0000

    Add best_answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,6 +6,7 @@ class AnswersController < ApplicationController
     answer.question_id = question.id
     answer.save
     AnswerScore.create(user_id:current_user.id, answer_id:answer.id, answer_score: 0)
+    question.update(question_status: "受付中")
     redirect_to question_path(question.id)
   end
 
@@ -14,11 +15,18 @@ class AnswersController < ApplicationController
     answer = Answer.find(params[:id])
     answer.update(answer_params)
     redirect_to question_path(question.id)
+  end
 
+  def bestanswer
+    question = Question.find(params[:question_id])
+    answer = Answer.find(params[:id])
+    answer.update(best_answer: true)
+    question.update(question_status: "解決済")
+    redirect_to question_path(question.id)
   end
 
   private
     def answer_params
-      params.require(:answer).permit(:user_id,  :question_id,  :answer_content, :best_answer)
+      params.require(:answer).permit(:user_id,  :question_id,  :answer_content, :best_answer, :best_answer)
     end
 end
