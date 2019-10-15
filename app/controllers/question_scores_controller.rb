97937425b7ea537cbc1de7887925935ed3b67bd8
Kenchi247 <inmf78bk247@gmail.com>
2019-10-15 04:35:28 +0000

    Add question score AJX

--- app/controllers/question_scores_controller.rb
+++ app/controllers/question_scores_controller.rb
@@ -1,21 +1,21 @@
 class QuestionScoresController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_question
   def create
-      question = Question.find(params[:question_id])
-      user = User.find_by(id: question.user_id)
-      question_score = QuestionScore.new(question_id: question.id, user_id: current_user)
+      question_score = QuestionScore.new(question_id: @question.id, user_id: current_user.id)
       question_score.save
-      score = user.score += 1
-      user.update(score: score)
-      redirect_to question_path(question.id)
+      score = @user.score += 1
+      @user.update(score: score)
   end
   def destroy
-      question = Question.find(params[:question_id])
-      user = User.find_by(id: question.user_id)
-      question_score = QuestionScore.find_by(question_id: question.id, user_id:current_user)
+      question_score = QuestionScore.find_by(question_id: @question.id, user_id: current_user.id)
       question_score.destroy
-      score = user.score -= 1
-      user.update(score: score)
-      redirect_to question_path(question.id)
+      score = @user.score -= 1
+      @user.update(score: score)
+  end
+  private
+  def set_question
+    @question = Question.find(params[:question_id])
+    @user = User.find_by(id: @question.user_id)
   end
 end
