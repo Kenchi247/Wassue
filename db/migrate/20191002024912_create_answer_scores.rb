d5f5aee8611fe60ef9a1d6f79e092f79ab0ef681
EC2 Default User <ec2-user@ip-172-31-19-130.us-east-2.compute.internal>
2019-10-19 09:00:18 +0000

    add develop

--- db/migrate/20191002024912_create_answer_scores.rb
+++ db/migrate/20191002024912_create_answer_scores.rb
@@ -1,6 +1,6 @@
 class CreateAnswerScores < ActiveRecord::Migration[5.2]
   def change
-    create_table :answer_scores do |t|
+    create_table :answer_scores, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.integer :user_id
       t.integer :answer_id
       t.integer :answer_score
--- db/migrate/20191002025311_create_examples.rb
+++ db/migrate/20191002025311_create_examples.rb
@@ -1,6 +1,6 @@
 class CreateExamples < ActiveRecord::Migration[5.2]
   def change
-    create_table :examples do |t|
+    create_table :examples, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.integer :user_id
       t.string :example_title
       t.text :example_content
