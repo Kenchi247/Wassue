7bfde12dcc091e1ace915fdcf7a4dc7a36fb94e2
EC2 Default User <ec2-user@ip-172-31-19-130.us-east-2.compute.internal>
2019-10-23 09:17:58 +0000

    Fix precompilw

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,6 +296,6 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-   config.omniauth :twitter, ENV['TWITTER_API'], ENV['TWITTER_API_SECRET'] 
+   config.omniauth :twitter, ENV['TWITTER_API'], ENV['TWITTER_API_SECRET'], oauth_callback: "#{ENV['HOST']}/users/auth/twitter/callback"
    config.omniauth :github, ENV['GITHUB_API'], ENV['GITHUBAPI_API_SECRET']
 end
