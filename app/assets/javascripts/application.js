4def9b52ab58de252cc7383ab935ab3c74bd5f4c
EC2 Default User <ec2-user@ip-172-31-37-3.us-east-2.compute.internal>
2019-11-05 04:34:48 +0000

    Update aboutpage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,9 @@
 //= require popper
 //= require bootstrap-sprockets
 
-Push.Permission.request();
+OneSignal.push(function() {
+  // Occurs when the user's subscription changes to a new value.
+  OneSignal.on('subscriptionChange', function (isSubscribed) {
+    console.log("The user's subscription state is now:", isSubscribed);
+  });
+});
